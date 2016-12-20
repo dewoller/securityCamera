2f9745e6025a69e962c243a460b9e2742354a2b4
dewoller <d.wollersheim@latrobe.edu.au>
2016-12-20 22:21:45 +1100

    convert model to float before save

--- model2ascii.lua
+++ model2ascii.lua
@@ -1,18 +1,22 @@
 
 mu=require('myUtilities.lua')
+require('nn')
+require('optim')
+require('dp')
 
 function saveModelASCII(imgSize, path, outFile )
   imgSize= imgSize or 128
   path = path or '/root/save/deep/E49/'
   outFile = outFile or "try.model"
-  model = torch.load( mu.getFile( path .. imgSize .. '/', '.*dat$')[1]):model()
+  model = torch.load( mu.getFile( path .. imgSize .. '/', '.*dat$')[1]):model():float()
   torch.save(outFile, model, 'ascii')
 end
 
 
-function resaveModel(infile, outFile )
-  outFile = outFile or "try.model"
-  model = torch.load( infile, 'ascii' )
+function resaveModel(inFile, outFile )
+  inFile = inFile or "try.model"
+  outFile = outFile or "try.t7"
+  model = torch.load( inFile, 'ascii' )
   torch.save(outFile, model)
 end
 
