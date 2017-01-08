3c91c4c4fdcf919458906de9711b92a2358b35b4
dewoller <d.wollersheim@latrobe.edu.au>
2017-01-09 07:07:17 +1100

    doall conv test

--- doall2.sh
+++ doall2.sh
@@ -2,12 +2,34 @@
 
 IMAGE=TorchGPU2
 
-docker exec -it $IMAGE ./deepExperiment.sh E49  --learningRate 0.01 --batchNorm --maxEpoch 5000 --maxTries 1000
-docker exec -it $IMAGE ./deepExperiment.sh E50  --learningRate 0.001 --momentum 0.5 --batchNorm --maxEpoch 5000 --maxTries 100
-docker exec -it $IMAGE ./deepExperiment.sh E51  --learningRate 0.001 --momentum 0.6 --batchNorm --maxEpoch 5000 --maxTries 100
-docker exec -it $IMAGE ./deepExperiment.sh E52  --learningRate 0.001 --momentum 0.7 --batchNorm --maxEpoch 5000 --maxTries 100
-docker exec -it $IMAGE ./deepExperiment.sh E53  --learningRate 0.001 --momentum 0.8 --batchNorm --maxEpoch 5000 --maxTries 100
-docker exec -it $IMAGE ./deepExperiment.sh E54  --learningRate 0.001 --momentum 0.9 --batchNorm --maxEpoch 5000 --maxTries 100
+docker exec -it $IMAGE ./convExperiment.sh E16 --learningRate 0.01 --maxEpoch 300 --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh E17 --learningRate 0.001 --maxEpoch 300 --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh E18 --learningRate 0.0001 --maxEpoch 300 --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh E19 --learningRate 0.1 --maxEpoch 300 --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh E20 --learningRate 0.001 --minLR 0.00000001 --maxEpoch 300 --lecunlcn --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh E21 --learningRate 0.001 --minLR 0.000000001 --maxEpoch 300 --lecunlcn --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh E22 --learningRate 0.001 --minLR 0.0000000001 --maxEpoch 300 --lecunlcn --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh E23 --learningRate 0.001 --minLR 0.00000000001 --maxEpoch 300 --lecunlcn --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh E24 --learningRate 0.001 --minLR 0.00000000001 --saturateEpoch 100 --lecunlcn --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh E25 --learningRate 0.1 --minLR 0.0000001 --saturateEpoch 100 --lecunlcn --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh E26 --learningRate 0.01 --maxEpoch 300 --lecunlcn --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh E27 --learningRate 0.001 --maxEpoch 300 --lecunlcn --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh E28 --learningRate 0.0001 --maxEpoch 300 --lecunlcn --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh E29 --learningRate 0.1 --maxEpoch 300 --lecunlcn --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh E30 --learningRate 0.001 --minLR 0.0000001 --maxEpoch 300 --lecunlcn --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh E31 --learningRate 0.001 --momentum 1 --maxEpoch 300 --lecunlcn --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh E32 --learningRate 0.001 --maxEpoch 300 --standardize --lecunlcn --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh E33 --learningRate 0.001 --maxEpoch 300 --zca --lecunlcn --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh E34 --learningRate 0.001 --maxEpoch 300 --lecunlcn --activation ReLU --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh E35 --learningRate 0.001 --maxEpoch 300 --lecunlcn --activation Sigmoid --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh E36 --learningRate 0.001 --maxEpoch 300 --lecunlcn --dropout
+docker exec -it $IMAGE ./convExperiment.sh E37 --learningRate 0.001 --padding --maxEpoch 300 --lecunlcn
+docker exec -it $IMAGE ./convExperiment.sh E38 --learningRate 0.001 --minLR 0.0000001 --lecunlcn --activation ReLU --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh E39 --learningRate 0.001 --minLR 0.0000001 --zca --lecunlcn --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh E40 --learningRate 0.001 --minLR 0.0000001 --standardize --lecunlcn --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh E41 --learningRate 0.001 --minLR 0.00000000001 --saturateEpoch 100 --standardize --lecunlcn --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh E42 --learningRate 0.001 --minLR 0.0000001 --batchSize 96 --lecunlcn --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh E43 --learningRate 0.001 --minLR 0.0000001 --batchSize 128 --lecunlcn --batchNorm
 
 
 
