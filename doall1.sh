207f09fb2e4c1f5260a7798ad3e65e86c50fa19d
dewoller <d.wollersheim@latrobe.edu.au>
2017-01-14 18:05:58 +1100

    saved parameters

--- doall1.sh
+++ doall1.sh
@@ -2,23 +2,51 @@
 
 IMAGE=TorchGPU1
 
+docker exec -it $IMAGE ./convExperiment.sh EH0 --learningRate 0.1 --maxEpoch 100 --activation Tanh --hiddenSize {4000,4000)
+docker exec -it $IMAGE ./convExperiment.sh EH1 --maxEpoch 100 --activation Tanh --hiddenSize {4000,4000) --dropout n
+docker exec -it $IMAGE ./convExperiment.sh EH2 --maxEpoch 10000 --activation Tanh --hiddenSize {4000,4000) --dropout n
+docker exec -it $IMAGE ./convExperiment.sh EH3 --maxEpoch 10000 --activation Tanh --hiddenSize {4000,4000) --dropout
+docker exec -it $IMAGE ./convExperiment.sh EH4 --maxEpoch 10000 --activation ReLU --hiddenSize {4000,4000) --dropout n
+docker exec -it $IMAGE ./convExperiment.sh EH5 --maxEpoch 10000 --activation ReLU --hiddenSize {4000,4000) --dropout
+docker exec -it $IMAGE ./convExperiment.sh EH6 --maxEpoch 10000 --activation Sigmoid --hiddenSize {4000,4000) --dropout n
+docker exec -it $IMAGE ./convExperiment.sh EH7 --maxEpoch 10000 --activation Sigmoid --hiddenSize {4000,4000) --dropout
+docker exec -it $IMAGE ./convExperiment.sh EH8 --hiddenSize {4000,4000)
+docker exec -it $IMAGE ./convExperiment.sh EH9 --zca --hiddenSize {4000,4000)
+docker exec -it $IMAGE ./convExperiment.sh EH10 --lecunlcn --hiddenSize {4000,4000)
+docker exec -it $IMAGE ./convExperiment.sh EH11 --zca --lecunlcn --hiddenSize {4000,4000)
+docker exec -it $IMAGE ./convExperiment.sh EH12 --standardize --hiddenSize {4000,4000)
+docker exec -it $IMAGE ./convExperiment.sh EH13 --standardize --zca --hiddenSize {4000,4000)
+docker exec -it $IMAGE ./convExperiment.sh EH14 --standardize --lecunlcn --hiddenSize {4000,4000)
+docker exec -it $IMAGE ./convExperiment.sh EH15 --standardize --zca --lecunlcn --hiddenSize {4000,4000)
+docker exec -it $IMAGE ./convExperiment.sh EH16 --learningRate 0.01 --maxEpoch 300 --hiddenSize {4000,4000) --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh EH17 --learningRate 0.001 --maxEpoch 300 --hiddenSize {4000,4000) --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh EH18 --learningRate 0.0001 --maxEpoch 300 --hiddenSize {4000,4000) --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh EH19 --learningRate 0.1 --maxEpoch 300 --hiddenSize {4000,4000) --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh EH20 --learningRate 0.001 --minLR 0.00000001 --maxEpoch 300 --lecunlcn --hiddenSize {4000,4000) --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh EH21 --learningRate 0.001 --minLR 0.000000001 --maxEpoch 300 --lecunlcn --hiddenSize {4000,4000) --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh EH22 --learningRate 0.001 --minLR 0.0000000001 --maxEpoch 300 --lecunlcn --hiddenSize {4000,4000) --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh EH23 --learningRate 0.001 --minLR 0.00000000001 --maxEpoch 300 --lecunlcn --hiddenSize {4000,4000) --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh EH24 --learningRate 0.001 --minLR 0.00000000001 --saturateEpoch 100 --lecunlcn --hiddenSize {4000,4000) --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh EH25 --learningRate 0.1 --minLR 0.0000001 --saturateEpoch 100 --lecunlcn --hiddenSize {4000,4000) --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh EH26 --learningRate 0.01 --maxEpoch 300 --lecunlcn --hiddenSize {4000,4000) --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh EH27 --learningRate 0.001 --maxEpoch 300 --lecunlcn --hiddenSize {4000,4000) --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh EH28 --learningRate 0.0001 --maxEpoch 300 --lecunlcn --hiddenSize {4000,4000) --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh EH29 --learningRate 0.1 --maxEpoch 300 --lecunlcn --hiddenSize {4000,4000) --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh EH30 --learningRate 0.001 --minLR 0.0000001 --maxEpoch 300 --lecunlcn --hiddenSize {4000,4000) --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh EH31 --learningRate 0.001 --momentum 1 --maxEpoch 300 --lecunlcn --hiddenSize {4000,4000) --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh EH32 --learningRate 0.001 --maxEpoch 300 --standardize --lecunlcn --hiddenSize {4000,4000) --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh EH33 --learningRate 0.001 --maxEpoch 300 --zca --lecunlcn --hiddenSize {4000,4000) --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh EH34 --learningRate 0.001 --maxEpoch 300 --lecunlcn --activation ReLU --hiddenSize {4000,4000) --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh EH35 --learningRate 0.001 --maxEpoch 300 --lecunlcn --activation Sigmoid --hiddenSize {4000,4000) --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh EH36 --learningRate 0.001 --maxEpoch 300 --lecunlcn --hiddenSize {4000,4000) --dropout
+docker exec -it $IMAGE ./convExperiment.sh EH37 --learningRate 0.001 --padding --maxEpoch 300 --lecunlcn --hiddenSize {4000,4000)
+docker exec -it $IMAGE ./convExperiment.sh EH38 --learningRate 0.001 --minLR 0.0000001 --lecunlcn --activation ReLU --hiddenSize {4000,4000) --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh EH39 --learningRate 0.001 --minLR 0.0000001 --zca --lecunlcn --hiddenSize {4000,4000) --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh EH40 --learningRate 0.001 --minLR 0.0000001 --standardize --lecunlcn --hiddenSize {4000,4000) --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh EH41 --learningRate 0.001 --minLR 0.00000000001 --saturateEpoch 100 --standardize --lecunlcn --hiddenSize {4000,4000) --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh EH42 --learningRate 0.001 --minLR 0.0000001 --batchSize 96 --lecunlcn --hiddenSize {4000,4000) --batchNorm
+docker exec -it $IMAGE ./convExperiment.sh EH43 --learningRate 0.001 --minLR 0.0000001 --batchSize 128 --lecunlcn --hiddenSize {4000,4000) --batchNorm
 
-docker exec -it $IMAGE ./convExperiment.sh E0 --learningRate 0.1 --maxEpoch 100 --activation Tanh
-docker exec -it $IMAGE ./convExperiment.sh E1 --maxEpoch 100 --activation Tanh --dropout n
-docker exec -it $IMAGE ./convExperiment.sh E2 --maxEpoch 10000 --activation Tanh --dropout n
-docker exec -it $IMAGE ./convExperiment.sh E3 --maxEpoch 10000 --activation Tanh --dropout
-docker exec -it $IMAGE ./convExperiment.sh E4 --maxEpoch 10000 --activation ReLU --dropout n
-docker exec -it $IMAGE ./convExperiment.sh E5 --maxEpoch 10000 --activation ReLU --dropout
-docker exec -it $IMAGE ./convExperiment.sh E6 --maxEpoch 10000 --activation Sigmoid --dropout n
-docker exec -it $IMAGE ./convExperiment.sh E7 --maxEpoch 10000 --activation Sigmoid --dropout
-docker exec -it $IMAGE ./convExperiment.sh E8
-docker exec -it $IMAGE ./convExperiment.sh E9 --zca
-docker exec -it $IMAGE ./convExperiment.sh E10 --lecunlcn
-docker exec -it $IMAGE ./convExperiment.sh E11 --zca --lecunlcn
-docker exec -it $IMAGE ./convExperiment.sh E12 --standardize
-docker exec -it $IMAGE ./convExperiment.sh E13 --standardize --zca
-docker exec -it $IMAGE ./convExperiment.sh E14 --standardize --lecunlcn
-docker exec -it $IMAGE ./convExperiment.sh E15 --standardize --zca --lecunlcn
 
 
 exec ./doall1a.sh
--- doall2.sh
+++ doall2.sh
@@ -2,34 +2,60 @@
 
 IMAGE=TorchGPU2
 
-docker exec -it $IMAGE ./convExperiment.sh E16 --learningRate 0.01 --maxEpoch 300 --batchNorm
-docker exec -it $IMAGE ./convExperiment.sh E17 --learningRate 0.001 --maxEpoch 300 --batchNorm
-docker exec -it $IMAGE ./convExperiment.sh E18 --learningRate 0.0001 --maxEpoch 300 --batchNorm
-docker exec -it $IMAGE ./convExperiment.sh E19 --learningRate 0.1 --maxEpoch 300 --batchNorm
-docker exec -it $IMAGE ./convExperiment.sh E20 --learningRate 0.001 --minLR 0.00000001 --maxEpoch 300 --lecunlcn --batchNorm
-docker exec -it $IMAGE ./convExperiment.sh E21 --learningRate 0.001 --minLR 0.000000001 --maxEpoch 300 --lecunlcn --batchNorm
-docker exec -it $IMAGE ./convExperiment.sh E22 --learningRate 0.001 --minLR 0.0000000001 --maxEpoch 300 --lecunlcn --batchNorm
-docker exec -it $IMAGE ./convExperiment.sh E23 --learningRate 0.001 --minLR 0.00000000001 --maxEpoch 300 --lecunlcn --batchNorm
-docker exec -it $IMAGE ./convExperiment.sh E24 --learningRate 0.001 --minLR 0.00000000001 --saturateEpoch 100 --lecunlcn --batchNorm
-docker exec -it $IMAGE ./convExperiment.sh E25 --learningRate 0.1 --minLR 0.0000001 --saturateEpoch 100 --lecunlcn --batchNorm
-docker exec -it $IMAGE ./convExperiment.sh E26 --learningRate 0.01 --maxEpoch 300 --lecunlcn --batchNorm
-docker exec -it $IMAGE ./convExperiment.sh E27 --learningRate 0.001 --maxEpoch 300 --lecunlcn --batchNorm
-docker exec -it $IMAGE ./convExperiment.sh E28 --learningRate 0.0001 --maxEpoch 300 --lecunlcn --batchNorm
-docker exec -it $IMAGE ./convExperiment.sh E29 --learningRate 0.1 --maxEpoch 300 --lecunlcn --batchNorm
-docker exec -it $IMAGE ./convExperiment.sh E30 --learningRate 0.001 --minLR 0.0000001 --maxEpoch 300 --lecunlcn --batchNorm
-docker exec -it $IMAGE ./convExperiment.sh E31 --learningRate 0.001 --momentum 1 --maxEpoch 300 --lecunlcn --batchNorm
-docker exec -it $IMAGE ./convExperiment.sh E32 --learningRate 0.001 --maxEpoch 300 --standardize --lecunlcn --batchNorm
-docker exec -it $IMAGE ./convExperiment.sh E33 --learningRate 0.001 --maxEpoch 300 --zca --lecunlcn --batchNorm
-docker exec -it $IMAGE ./convExperiment.sh E34 --learningRate 0.001 --maxEpoch 300 --lecunlcn --activation ReLU --batchNorm
-docker exec -it $IMAGE ./convExperiment.sh E35 --learningRate 0.001 --maxEpoch 300 --lecunlcn --activation Sigmoid --batchNorm
-docker exec -it $IMAGE ./convExperiment.sh E36 --learningRate 0.001 --maxEpoch 300 --lecunlcn --dropout
-docker exec -it $IMAGE ./convExperiment.sh E37 --learningRate 0.001 --padding --maxEpoch 300 --lecunlcn
-docker exec -it $IMAGE ./convExperiment.sh E38 --learningRate 0.001 --minLR 0.0000001 --lecunlcn --activation ReLU --batchNorm
-docker exec -it $IMAGE ./convExperiment.sh E39 --learningRate 0.001 --minLR 0.0000001 --zca --lecunlcn --batchNorm
-docker exec -it $IMAGE ./convExperiment.sh E40 --learningRate 0.001 --minLR 0.0000001 --standardize --lecunlcn --batchNorm
-docker exec -it $IMAGE ./convExperiment.sh E41 --learningRate 0.001 --minLR 0.00000000001 --saturateEpoch 100 --standardize --lecunlcn --batchNorm
-docker exec -it $IMAGE ./convExperiment.sh E42 --learningRate 0.001 --minLR 0.0000001 --batchSize 96 --lecunlcn --batchNorm
-docker exec -it $IMAGE ./convExperiment.sh E43 --learningRate 0.001 --minLR 0.0000001 --batchSize 128 --lecunlcn --batchNorm
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden1  --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden2  --learningRate 0.001 --dropout --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden3  --learningRate 0.0001 --dropout --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden4  --learningRate 0.00001 --dropout --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden5  --learningRate 0.00001 --dropout --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden6  --learningRate 0.00001 --dropout --standardize --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden7  --learningRate 0.00001 --dropout --zca --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden8  --learningRate 0.00001 --dropout --lecunlcn --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden9  --learningRate 0.00001 --batchNorm --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden10  --learningRate 0.00001 --maxOutNorm 2 --dropout --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden11  --learningRate 0.1 --batchNorm --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden12  --learningRate 0.01 --batchNorm --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden13  --learningRate 0.001 --batchNorm --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden14  --learningRate 0.0001 --batchNorm --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden15  --learningRate 0.00001 --batchNorm --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden16  --learningRate 0.000001 --batchNorm --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden17  --learningRate 0.1 --batchNorm --standardize --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden18  --learningRate 0.01 --batchNorm --standardize --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden19  --learningRate 0.001 --batchNorm --standardize --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden20  --learningRate 0.0001 --batchNorm --standardize --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden21  --learningRate 0.00001 --batchNorm --standardize --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden22  --learningRate 0.000001 --batchNorm --standardize --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden23  --learningRate 0.1 --batchNorm --zca --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden24  --learningRate 0.01 --batchNorm --zca --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden25  --learningRate 0.001 --batchNorm --zca --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden26  --learningRate 0.0001 --batchNorm --zca --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden27  --learningRate 0.00001 --batchNorm --zca --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden28  --learningRate 0.000001 --batchNorm --zca --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden29  --learningRate 0.1 --batchNorm --lecunlcn --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden30  --learningRate 0.01 --batchNorm --lecunlcn --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden31  --learningRate 0.001 --batchNorm --lecunlcn --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden32  --learningRate 0.0001 --batchNorm --lecunlcn --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden33  --learningRate 0.00001 --batchNorm --lecunlcn --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden34  --learningRate 0.000001 --batchNorm --lecunlcn --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden35  --learningRate 0.01 --batchSize 64 --batchNorm --hiddenSize {4000,4000} --accUpdate
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden36  --learningRate 0.01 --batchSize 128 --batchNorm --hiddenSize {4000,4000} --accUpdate
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden37  --learningRate 0.01 --momentum 0.5 --batchNorm --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden38  --learningRate 0.01 --momentum 1 --batchNorm --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden39  --learningRate 0.01 --momentum 2 --batchNorm --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden40  --learningRate 0.01 --momentum 4 --batchNorm --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden41  --learningRate 0.01 --maxOutNorm 1 --batchNorm --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden42  --learningRate 0.01 --maxOutNorm 2 --batchNorm --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden43  --learningRate 0.01 --maxOutNorm 3 --batchNorm --hiddenSize {4000,4000}
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden44  --learningRate 0.01 --maxOutNorm 4 --batchNorm --hiddenSize {4000,4000} --maxTries 100
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden45  --learningRate 0.01 --batchNorm --hiddenSize {4000,4000} --maxTries 500
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden46  --learningRate 0.01 --batchNorm --hiddenSize {4000,4000} --maxTries 1000
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden47  --learningRate 0.01 --batchNorm --hiddenSize {4000,4000} --maxEpoch 5000 --maxTries 100
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden48  --learningRate 0.01 --batchNorm --hiddenSize {4000,4000} --maxEpoch 5000 --maxTries 500
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden49  --learningRate 0.01 --batchNorm --hiddenSize {4000,4000} --maxEpoch 5000 --maxTries 1000
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden50  --learningRate 0.001 --momentum 0.5 --batchNorm --hiddenSize {4000,4000} --maxEpoch 5000 --maxTries 100
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden51  --learningRate 0.001 --momentum 0.6 --batchNorm --hiddenSize {4000,4000} --maxEpoch 5000 --maxTries 100
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden52  --learningRate 0.001 --momentum 0.7 --batchNorm --hiddenSize {4000,4000} --maxEpoch 5000 --maxTries 100
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden53  --learningRate 0.001 --momentum 0.8 --batchNorm --hiddenSize {4000,4000} --maxEpoch 5000 --maxTries 100
+docker exec -it $IMAGE ./deepExperiment.sh DeepHidden54  --learningRate 0.001 --momentum 0.9 --batchNorm --hiddenSize {4000,4000} --maxEpoch 5000 --maxTries 100
 
 
 
