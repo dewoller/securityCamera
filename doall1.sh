da913ead3042662eb08a2758b2758b66ddbfbdfe
dewoller <d.wollersheim@latrobe.edu.au>
2017-01-28 11:23:40 +1100

    smaller 2 category experiment setup

--- doall1.sh
+++ doall1.sh
@@ -2,7 +2,7 @@
 
 IMAGE=TorchGPU1
 
-docker exec -it $IMAGE ./convExperiment.sh EH44 --learningRate 0.001 --minLR 0.0000001 --standardize --zca --lecunlcn --hiddenSize '{400}' --batchNorm --dropout
+docker exec -it $IMAGE ./convExperiment2.sh EH_2_44 --learningRate 0.001 --minLR 0.0000001 --standardize --zca --lecunlcn --hiddenSize '{400}' --batchNorm --dropout
 
 docker exec -it $IMAGE ./convExperiment2.sh EH_2_40 --learningRate 0.001 --minLR 0.0000001 --standardize --lecunlcn --hiddenSize '{4000,4000}' --batchNorm
 docker exec -it $IMAGE ./convExperiment2.sh EH_2_41 --learningRate 0.001 --minLR 0.00000000001 --saturateEpoch 100 --standardize --lecunlcn --hiddenSize '{4000,4000}' --batchNorm
